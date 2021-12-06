8e94a5e3fed9e722a20cc8c429f2d7743cc6895b
Erghel <88831850+Erghel@users.noreply.github.com>
2021-12-06 13:47:57 +0300

    Спасибо, Саня <3

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
 function generatePassword() {
-  let length = 23;
-  let chars = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-()`;
-  chars = chars.replace('\n', " ").replace(" ", '');
-  let password = "";
-  let n = chars.length;
-  for (var i = 0; i < length; ++i) {
-      password += chars.charAt(Math.floor(Math.random() * n));
-  }
-  const passwordInput = document.getElementById('password');
-  passwordInput.value = password;
+    let length = 25;
+    let chars = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-()`;
+    chars = chars.replace('\n', " ").replace(" ", '');
+    let password = "";
+    let n = chars.length;
+    for (var i = 0; i < length; ++i) {
+        password += chars.charAt(Math.floor(Math.random() * n));
+    }
+    const passwordInput = document.getElementById('password');
+    passwordInput.value = password;
 }
-/**function copytext() {
-  navigator.clipboard.writeText(password);
-  info.style.opacity = "1";
-  setTimeout(function() {
-    info.style.opacity = "0"
-  }, 1000);
-}*/
+
+function fallbackCopyTextToClipboard(text) {
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+  
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+   let successful = document.execCommand('copy');
+   let msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+function copytext() {
+  const text = document.getElementById('password').value
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    info.style.opacity = "1";
+    setTimeout(function() {
+      info.style.opacity = "0"
+    }, 1000);
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
+
+document.getElementById('copy').addEventListener('click', function(event) {
+  copytext();
+});
